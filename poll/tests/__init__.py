fae9a80389a2ea1df11e4d3afd3195abd641948b
Jason Paidoussi <jason@paidoussi.net>
2014-12-15 13:53:18 +0200

    Fixed test and added travis support

--- poll/tests/__init__.py
+++ poll/tests/__init__.py
@@ -21,35 +21,34 @@ class TestCase(TestCase):
         cls.editor.set_password("password")
         cls.editor.save()
 
+    def test_add_vote(self):
         # poll
-        obj, dc = Poll.objects.get_or_create(
+        poll, dc = Poll.objects.get_or_create(
             title='Poll',
-            owner=cls.editor, state='published',
+            owner=self.editor, state='published',
             comments_enabled=False,
         )
-        obj.sites = [1]
-        obj.save()
-        cls.poll = obj
+        poll.sites = [1]
+        poll.save()
 
         option_one = PollOption(
             title='Option One',
-            poll=cls.poll
+            poll=poll
         )
         option_two = PollOption(
             title='Option Two',
-            poll=cls.poll
+            poll=poll
         )
         option_one.save()
         option_two.save()
 
-    def test_add_vote(self):
-        self.client.login(username='editor', password='password')
         response = self.client.post(
-            reverse('poll-detail-vote', kwargs={'poll_id': self.poll.id}),
-            {'poll_option': 1}
+            reverse('poll-detail-vote', kwargs={'poll_id': poll.id}),
+            {'poll_option': 1},
+            REMOTE_ADDR='127.0.0.1',
+            HTTP_USER_AGENT='testclient'
         )
         self.assertEqual(response.status_code, 200)
-        # import ipdb; ipdb.set_trace()
         self.failUnless(
-            "Your vote has been saved" in response.content
+            "poll-detail-results" in response.content
         )
